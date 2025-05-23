4f4bf17409abde808b7740f423438526af0ecbf5
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2025-05-23 11:43:38 -0500

    not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,20 @@
+import Bounded from '@/components/Bounded';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
-const NotFound = () => {
-  return <div>NotFound</div>;
-};
-
-export default NotFound;
+export default function NotFound() {
+  return (
+    <Bounded className="py-10 flex flex-col gap-5 justify-center items-center">
+      The URL you are looking for is not found.
+      <div className="flex justify-center items-center">
+        <p className="text-fs-800 text-brand-light-green font-bold text-center">
+          404...
+        </p>
+      </div>
+      <Button>
+        <Link href="/">Back to Homepage</Link>
+      </Button>
+    </Bounded>
+  );
+}
